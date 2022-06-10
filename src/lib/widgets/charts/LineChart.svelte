cd3d0bd9849f21b92dbb874f9bd72366ee4d012a
filip <filiptangen1998@live.com>
2022-06-10 12:54:56 +0200

    charts and stuff

--- src/lib/widgets/charts/LineChart.svelte
+++ src/lib/widgets/charts/LineChart.svelte
@@ -1,14 +1,20 @@
 <script>
 	// 	Note: Due to REPL limitations, full responsiveness may not work here. Download the example from here or from the website (https://layercake.graphics/example/Line) and run locally to get all features.
 
-	import { LayerCake, Svg } from 'layercake';
+	import { LayerCake, Svg, Html } from 'layercake';
+	import { timeParse, timeFormat } from 'd3-time-format';
+	import { format, precisionFixed } from 'd3-format';
 
 	import Line from './utils/Line.svelte';
 	import Area from './utils/Area.svelte';
 	import AxisX from './utils/AxisX.svelte';
 	import AxisY from './utils/AxisY.svelte';
 
+	import Labels from './utils/GroupLabels.html.svelte';
+	import SharedTooltip from './utils/SharedTooltip.html.svelte';
+
 	import data from './utils/points.js';
+	import data2 from './utils/fruit.js';
 
 	const xKey = 'myX';
 	const yKey = 'myY';
@@ -16,6 +22,8 @@
 	data.forEach((d) => {
 		d[yKey] = +d[yKey];
 	});
+	const formatTickX = timeFormat('%b. %e');
+	const formatTickY = (d) => format(`.${precisionFixed(d)}s`)(d);
 </script>
 
 <div class=" w-full h-full card shadow p-2">
@@ -28,13 +36,14 @@
 	>
 		<Svg>
 			<AxisX />
-			<AxisY ticks={4} />
+			<AxisY ticks={4} formatTick={formatTickY} />
 			<Line />
 			<Area />
 		</Svg>
+		<!--
 		<Html>
 			<Labels />
 			<SharedTooltip formatTitle={formatTickX} dataset={data} />
-		</Html>
+		</Html>-->
 	</LayerCake>
 </div>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -2,18 +2,17 @@
 	import StatsAndIcon from '$lib/widgets/StatsAndIcon.svelte';
 	import Stats from '$lib/widgets/Stats.svelte';
 	import getAirQuality from '$lib/api/air_quality.js';
-import { onMount } from 'svelte';
+	import LineChart from '$lib/widgets/charts/LineChart.svelte';
 
-	let station = 'E18+Høvik+kirke'
-	let airQuality = {'PM10': '0', 'NO2': '0', 'PM2.5': '0'};
+	let station = 'E18+Høvik+kirke';
+	let airQuality = { PM10: '0', NO2: '0', 'PM2.5': '0' };
 
-	$: getAirQuality(station).then(data => {
+	$: getAirQuality(station).then((data) => {
 		airQuality = data;
 	});
-	
 </script>
 
-<div class="grid grid-cols-1 lg:grid-cols-12 gap-2 p-4">
+<div class="grid grid-cols-1 lg:grid-cols-12 gap-2 p-4 items-center">
 	<div class="lg:col-span-6">
 		<div class="font-extrabold md:text-5xl text-3xl text-center">Bærum kommune i dag</div>
 	</div>
@@ -41,22 +40,25 @@ import { onMount } from 'svelte';
 		<StatsAndIcon title="Sykkelpasseringer" value="219" icon="fa-person-biking text-success" />
 	</div>
 
-	<div class=lg:col-span-4>
-	  <select class="select w-full max-w-xs" bind:value={station}>
-		<option disabled selected>Velg målestasjon</option>
-		<option value="E18+Høvik+kirke">E18 Høvik kirke</option>
-		<option value="Bekkestua">Bekkestua</option>
-	  </select>
+	<div class="lg:col-span-4">
+		<select class="select w-full max-w-xs" bind:value={station}>
+			<option disabled selected>Velg målestasjon</option>
+			<option value="E18+Høvik+kirke">E18 Høvik kirke</option>
+			<option value="Bekkestua">Bekkestua</option>
+		</select>
 	</div>
 	<div class="lg:col-span-2">
-		<StatsAndIcon title="NO2 (ug/m³)" value="{airQuality?.NO2}" icon="fa-smile text-success" />
-	</div>
-
-	<div class="lg:col-span-2">
-		<StatsAndIcon title="PM2,5 (ug/m³)" value="{airQuality['PM2.5']}" icon="fa-smile text-success" />
+		<StatsAndIcon title="NO2 (ug/m³)" value={airQuality?.NO2} icon="fa-smile text-success" />
 	</div>
 
 	<div class="lg:col-span-2">
-		<StatsAndIcon title="PM10 (ug/m³)" value="{airQuality?.PM10}" icon="fa-smile text-success" />
+		<StatsAndIcon title="PM2,5 (ug/m³)" value={airQuality['PM2.5']} icon="fa-smile text-success" />
+	</div>
+
+	<div class="lg:col-span-2">
+		<StatsAndIcon title="PM10 (ug/m³)" value={airQuality?.PM10} icon="fa-smile text-success" />
+	</div>
+	<div class="lg:col-span-6 h-60">
+		<LineChart />
 	</div>
 </div>
