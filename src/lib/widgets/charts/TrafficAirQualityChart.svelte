f97cbd92099d0e677b8be25a4da321cde8b95b86
filip <filiptangen1998@live.com>
2022-06-13 10:13:39 +0200

    fixed

--- src/lib/widgets/charts/TrafficAirQualityChart.svelte
+++ src/lib/widgets/charts/TrafficAirQualityChart.svelte
@@ -1,44 +1,42 @@
 <script>
-    import { getAirQualityInterval } from "$lib/api/air_quality";
-    import { onMount } from "svelte";
-    import LineChart from "./LineChart.svelte";
+	import { getAirQualityInterval } from '$lib/api/air_quality';
+	import { onMount } from 'svelte';
+	import LineChart from './LineChart.svelte';
 
-    export let startTime = "";
-    export let endTime = "";
-    export let aqStation = "";
+	export let startTime = '';
+	export let endTime = '';
+	export let aqStation = '';
 
-    let data = undefined;
-    let labels = undefined;
+	let datasets = undefined;
+	let labels = undefined;
 
-    onMount(async () => {
+	onMount(async () => {
 		let aqData = await getAirQualityInterval(aqStation, startTime, endTime);
-        labels = aqData.NO2.map(d => d.toTime);
+		labels = aqData.NO2.map((d) => d.toTime);
 
-        let NO2Values = aqData.NO2.map(d => d.value);
-        let PM25Values = aqData['PM2.5'].map(d => d.value);
-        let PM10Values = aqData.PM10.map(d => d.value);
-        
-        data = {
-            labels: labels,
-            dataset: [
-                {
-                    // label: "NO2",
-                    values: NO2Values,
-                },
-                {
-                    // label: "PM2.5",
-                    values: PM25Values,
-                },
-                {
-                    // label: "PM10",
-                    values: PM10Values,
-                },
-            ]
-        };
-        console.log(data);
+		let NO2Values = aqData.NO2.map((d) => d.value);
+		let PM25Values = aqData['PM2.5'].map((d) => d.value);
+		let PM10Values = aqData.PM10.map((d) => d.value);
+
+		datasets = [
+			{
+				// label: "NO2",
+				values: NO2Values
+			},
+			{
+				// label: "PM2.5",
+				values: PM25Values
+			},
+			{
+				// label: "PM10",
+				values: PM10Values
+			}
+		];
 	});
 </script>
 
 <div class="stats shadow w-full h-full">
-	<LineChart dataset={data} labels={labels}/>
+	{#key datasets}
+		<LineChart {datasets} {labels} />
+	{/key}
 </div>
